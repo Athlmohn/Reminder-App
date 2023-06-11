8e55b09996bb4fdcd9229686c48d6df657a78632
Athlmohn <athulmnair01@gmail.com>
2023-06-11 17:54:27 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Reminder from './components/Reminder';
+
+
+function App() {
+  return (
+    <div>
+     <Reminder/>
+    </div>
+  );
+}
+
+export default App;
